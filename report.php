1d81a136a99d7fa60725983e2dc6828e31c5fca1
M-A-R-S1 <marc-andre.schmidt@ruhr-uni-bochum.de>
2026-05-03 21:31:33 +0200

    Added Text and Emoji types to the report | Using multilang strings

--- report.php
+++ report.php
@@ -26,6 +26,9 @@
 require_once(__DIR__ . '/../../config.php');
 global $CFG, $DB, $OUTPUT, $PAGE;
 
+use block_coursefeedback\local\multilang_string;
+use block_coursefeedback\local\surveyitem\emoji\emoji_surveyitem;
+
 require_login();
 $context = context_system::instance();
 $context = context_system::instance();
@@ -39,16 +42,22 @@ echo $OUTPUT->header();
 
 # Get the Survey id from this course. [There can be only one survey in a course ... can it?]
 $survey_execution_data = $DB->get_record('block_coursefeedback_surveyexecution', ['courseid' => $course_id]);
+
 # Get All survey parts.
-$survey_execution_parts = $DB->get_records('block_coursefeedback_surveypartexecution', ['surveyexecutionid' => $survey_execution_data->id]);
+if($survey_execution_data) {
+    $survey_execution_parts = $DB->get_records('block_coursefeedback_surveypartexecution', ['surveyexecutionid' => $survey_execution_data->id]);
+} else {
+    $survey_execution_parts = [];
+}
+
+$renderdata = [];
 
 # loop through the parts and display them each as a card!
 foreach ($survey_execution_parts as $survey_execution_part) {
     # Save the survey part id [This id helps to identify the used survey questions].
     $survey_part_id = $survey_execution_part->surveypartid;
     # Get all survey questions which are connected to this part id.
-    $survey_item_data = $DB->get_records('block_coursefeedback_surveyitem', ['surveypartid' => $survey_part_id]);
-
+    $survey_item_data = $DB->get_records('block_coursefeedback_surveyitem', ['surveypartid' => $survey_part_id], 'sortindex ASC');
     # Get the ids of all users which have submitted this part of the survey.
     $survey_anonym_user_data = $DB->get_records('block_coursefeedback_surveypartexecutionoptionresp', ['surveypartexecutionoptionid' => $survey_execution_part->id]);
     # Extract the ids.
@@ -56,22 +65,25 @@ foreach ($survey_execution_parts as $survey_execution_part) {
 
     # Get all responses from these users.
     list($insql, $params) = $DB->get_in_or_equal($user_response_ids);
-    $sql = "SELECT id, surveyitemid, value FROM {block_coursefeedback_surveyitemintresponse} WHERE surveypartexecutionoptionresponseid $insql ORDER BY surveyitemid ASC";
+    $sql = "SELECT id, surveyitemid, value FROM {block_coursefeedback_surveyitemintresponse} WHERE surveypartexecutionoptionresponseid $insql";
     $responses = $DB->get_records_sql($sql, $params);
+    $sql_text = "SELECT id, surveyitemid, value FROM {block_coursefeedback_surveyitemtextresponse} WHERE surveypartexecutionoptionresponseid $insql";
+    $text_responses = $DB->get_records_sql($sql_text, $params);
 
     $questions_data = [];
 
     # Loop through all items of the survey.
     foreach ($survey_item_data as $item) {
         # Collect only data from singlechoice and scale questions.
-        $supported_types = ['singlechoice', 'scalequestion'];
+        $supported_types = ['singlechoice', 'scalequestion', 'multiplechoice', 'emoji', 'text'];
         if (!in_array($item->surveyitemtype, $supported_types)) {
             continue;
         }
 
-        // Remove html tags which are in the database ... .. .
-        $text_record = $DB->get_record('block_coursefeedback_texttranslation', ['id' => $item->textid]);
-        $current_question_text = $text_record ? strip_tags($text_record->text) : "- Leeres Textelement -";
+        # Get the question text.
+        $raw_question_text = multilang_string::deserialize($item->text);
+        $current_question_text = strip_tags($raw_question_text->translate());
+        $current_question_text = strip_tags($raw_question_text->translate());
 
         $counts = [];
         $maxOptions = 0;
@@ -86,8 +98,22 @@ foreach ($survey_execution_parts as $survey_execution_part) {
                     $counts[$i] = 0;
                 }
             }
+        } # Emoji handling ...
+        else if ($item->surveyitemtype == 'emoji') {
+            $emoji_record = $DB->get_record('block_coursefeedback_surveyitememojis', ['surveyitemid' => $item->id]);
+
+            if ($emoji_record) {
+                $variants = emoji_surveyitem::get_available_variants();
+                $variant = $variants[$emoji_record->variant] ?? null;
+                if ($variant) {
+                    foreach ($variant['choices'] as $choice) {
+                        $counts[(int)$choice['value']] = 0;
+                    }
+                    $maxOptions = count($variant['choices']);
+                }
+            }
         } # Singlechoice question handling ...
-        else if ($item->surveyitemtype == 'singlechoice') {
+        else if ($item->surveyitemtype == 'singlechoice' || $item->surveyitemtype == 'multiplechoice') {
             $options = $DB->get_records('block_coursefeedback_surveyitemansweroption', ['surveyitemid' => $item->id], 'sortindex ASC');
             foreach ($options as $opt) {
                 $counts[$opt->id] = 0;
@@ -100,6 +126,7 @@ foreach ($survey_execution_parts as $survey_execution_part) {
             'questiontext' => $current_question_text,
             'max_options' => $maxOptions,
             'counts' => $counts,
+            'responses' => [],
             'n' => 0
         ];
     }
@@ -117,6 +144,14 @@ foreach ($survey_execution_parts as $survey_execution_part) {
         }
     }
 
+    foreach ($text_responses as $text_res) {
+        $itemId = $text_res->surveyitemid;
+        if (isset($questions_data[$itemId])) {
+            $questions_data[$itemId]['responses'][] = ['text' => s($text_res->value)];
+            $questions_data[$itemId]['n']++;
+        }
+    }
+
     # D3 plotting values here.
     foreach ($questions_data as $itemId => &$data) {
         $legend = [];
@@ -127,48 +162,72 @@ foreach ($survey_execution_parts as $survey_execution_part) {
             if ($mapping) {
                 $scale = $DB->get_record('block_coursefeedback_scale', ['id' => $mapping->scaleid]);
                 $data['scale_name'] = $scale ? $scale->name : '';
+                $data['scale_min_name'] = strip_tags(multilang_string::deserialize($scale->minoptiontext)->translate());
+                $data['scale_max_name'] = strip_tags(multilang_string::deserialize($scale->maxoptiontext)->translate());
             }
         } # Singlechoice ...
-        else if ($data['type'] === 'singlechoice') {
+        else if ($data['type'] === 'singlechoice' || $data['type'] === 'multiplechoice') {
             $options = $DB->get_records('block_coursefeedback_surveyitemansweroption', ['surveyitemid' => $itemId], 'sortindex ASC');
             $index = 1;
             foreach ($options as $opt) {
-                $translation = $DB->get_record('block_coursefeedback_texttranslation', ['id' => $opt->textid]);
                 $legend[] = [
                     'number' => $index++,
-                    'text' => $translation ? strip_tags($translation->text) : "???"
+                    'text' => strip_tags(multilang_string::deserialize($opt->text)->translate())
                 ];
+            }
+        } # Emoji ...
+        else if ($data['type'] === 'emoji') {
+            $emoji_record = $DB->get_record('block_coursefeedback_surveyitememojis', ['surveyitemid' => $itemId]);
+            $variants = emoji_surveyitem::get_available_variants();
+            $variant = $variants[$emoji_record->variant] ?? null;
+
+            if ($variant) {
+                $data['scale_name'] = $variant['name'];
+                $data['scale_min_name'] = $variant['choices'][0]['emoji'];
+                $data['scale_max_name'] = end($variant['choices'])['emoji'];
             }
         }
         $data['legend'] = $legend;
     }
-
     $questions_for_template = [];
+    $question_index = 1;
 
-    foreach ($questions_data as $itemId => $data) {
+    foreach ($questions_data as $itemId => &$data) {
         # Calculate statistic values here ...
-        $is_scale = ($data['type'] === 'scalequestion');
-        $stats = calculate_survey_stats(array_values($data['counts']), 1);
+        $is_scale = ($data['type'] === 'scalequestion' || $data['type'] === 'emoji');
+        $is_text = ($data['type'] === 'text');
+
+        if ($is_text) {
+            $stats = [
+                'n' => $data['n'],
+                'mean' => 0,
+                'median' => 0,
+                'stddev' => 0
+            ];
+        } else {
+            $stats = calculate_survey_stats(array_values($data['counts']), 1);
+        }
 
         $percents = [];
         foreach ($data['counts'] as $c) {
             $percents[] = ($stats['n'] > 0) ? round(($c / $stats['n']) * 100, 1) : 0;
         }
 
-        # Remove html tags.
-        $clean_text = strip_tags($data['questiontext']);
-
         $questions_for_template[] = [
             'itemid' => $itemId,
-            'questiontext' => $clean_text,
+            'index' => $question_index,
+            'questiontext' => $data['questiontext'],
             'n' => $stats['n'],
             'mean' => $is_scale ? $stats['mean'] : null,
             'median' => $is_scale ? $stats['median'] : null,
             'stddev' => $is_scale ? $stats['stddev'] : null,
             'has_stats' => $is_scale && ($stats['n'] > 0),
-            'scale_name' => $data['scale_name'] ?? '',
+            'is_scale' => $is_scale,
+            'scale_min_name' => $data['scale_min_name'] ?? '',
+            'scale_max_name' => $data['scale_max_name'] ?? '',
             'legend' => $data['legend'],
-            'is_singlechoice' => ($data['type'] === 'singlechoice'),
+            'is_text' => $is_text,
+            'responses' => $data['responses'],
             'counts_length' => count($data['counts']),
             'chart_data_json' => json_encode([
                 'itemid' => (int)$itemId,
@@ -181,6 +240,8 @@ foreach ($survey_execution_parts as $survey_execution_part) {
                 'n' => (int)$stats['n']
             ])
         ];
+
+        $question_index++;
     }
 
     $renderdata = ['questions' => $questions_for_template];
