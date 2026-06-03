f52df5f07adf65dde823c6d48352934a9e1f0929
Maximilian Haye <m.haye@tu-berlin.de>
2026-06-03 14:58:39 +0200

    feat: allow showing a label for the center option of a scale

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_coursefeedback';
-$plugin->release = '4.0.0 (Build: 2026060101)';
-$plugin->version = 2026060101;
+$plugin->release = '4.0.0 (Build: 2026061700)';
+$plugin->version = 2026061700;
 $plugin->requires = 2022042208;
 $plugin->maturity = MATURITY_ALPHA;
