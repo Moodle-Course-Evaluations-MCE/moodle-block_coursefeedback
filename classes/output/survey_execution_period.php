6d169be05550dab32ea2e78a9a22d8214b8413f2
Maximilian Haye <m.haye@tu-berlin.de>
2026-06-09 11:00:51 +0200

    chore: correct copyright header
    We're not in QuestionPy anymore

--- classes/output/survey_execution_period.php
+++ classes/output/survey_execution_period.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of the QuestionPy Moodle plugin - https://questionpy.org
+// This file is part of Moodle - https://questionpy.org
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
